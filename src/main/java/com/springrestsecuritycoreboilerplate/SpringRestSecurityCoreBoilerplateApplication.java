0e0a7c80d7aa4224b2d1c36c1e34cb76bdfd4a0e
Volkan <volkandogan.d@gmail.com>
2019-11-30 11:19:40 +0300

    Initial commit & create a project with some dependencies

---
+++ src/main/java/com/springrestsecuritycoreboilerplate/SpringRestSecurityCoreBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package com.springrestsecuritycoreboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSecurityCoreBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSecurityCoreBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrestsecuritycoreboilerplate/SpringRestSecurityCoreBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrestsecuritycoreboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSecurityCoreBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
