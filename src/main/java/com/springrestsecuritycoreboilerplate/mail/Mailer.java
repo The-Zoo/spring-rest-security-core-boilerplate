c01fef4287b5a09979ef4fe9fcd2f4b1465e8ece
Volkan Dogan <volkandogan.d@gmail.com>
2020-02-08 20:40:53 +0300

    Uncomment for one function

--- src/main/java/com/springrestsecuritycoreboilerplate/mail/Mailer.java
+++ src/main/java/com/springrestsecuritycoreboilerplate/mail/Mailer.java
@@ -26,16 +26,14 @@ public class Mailer implements Serializable {
 
 	public void sendVerificationEmailMessage(final AppUser user, final VerificationToken verificationToken,
 			final String mailSubject) {
-
-//		final String recipientAddress = user.getEmail();
-//		final String subject = mailSubject;
-//		final SimpleMailMessage email = new SimpleMailMessage();
-//		email.setTo(recipientAddress);
-//		email.setSubject(subject);
-//		email.setText("Token is " + verificationToken.getToken());
-//		email.setFrom(env.getProperty("support.email"));
-//		mailSender.send(email);
-
+		final String recipientAddress = user.getEmail();
+		final String subject = mailSubject;
+		final SimpleMailMessage email = new SimpleMailMessage();
+		email.setTo(recipientAddress);
+		email.setSubject(subject);
+		email.setText("Token is " + verificationToken.getToken());
+		email.setFrom(env.getProperty("support.email"));
+		mailSender.send(email);
 	}
 
 }
